1a52ea0ccd33dc2b759402496b99e0783ee1dd17
issu478 <issumr860@gmail.com>
2026-01-13 09:28:48 +0530

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "OJ12iCxI#sol8WO7vTF1TqZ_xnMqSvENhWXIVGaXtMpdsZfy5FKc",
+SESSION_ID: process.env.SESSION_ID || "aYkEQTwQ#xqkXhpjXTcnal_IjTrnXUmRTmVKBs8ZE_JIB82QmioQ",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://files.catbox.moe/4bc81k.png",
 ALIVE_MSG : process.env.ALIVE_MSG || "👋 *Hello, ${pushname}\n I'm alive now 😼 \n Pleace type .menu to see full cammand list . \n © QUEEN-MAYA-MD Is a third party program to improve Your WhatsApp expireance fun and useful.\n 😼 I was created by Sandes isuranda . A talented developer with different inovations . \n QUEEN-MAYA-MD 2025 ♠ All rights ricived ♠ \n > Powered by Sndes isuranda ツ",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
