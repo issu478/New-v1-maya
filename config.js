475d79aa8d9d4f190a8a47a93cf5c0a3313dd288
issu478 <issumr860@gmail.com>
2026-01-14 15:36:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "TdkR0aCQ#byanvG8kZgceLFraKMBatls74QrSoIBUE9cPcKW7kCA",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://files.catbox.moe/4bc81k.png",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/logo.webp",
 ALIVE_MSG : process.env.ALIVE_MSG || "👋 *Hello, ${pushname}\n I'm alive now 😼 \n Pleace type .menu to see full cammand list . \n © QUEEN-MAYA-MD Is a third party program to improve Your WhatsApp expireance fun and useful.\n 😼 I was created by Sandes isuranda . A talented developer with different inovations . \n QUEEN-MAYA-MD 2025 ♠ All rights ricived ♠ \n > Powered by Sndes isuranda ツ",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
